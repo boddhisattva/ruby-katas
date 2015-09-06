2dd89631c5070146723d522f77652630868fd419
Mohnish Jadwani <mail4mohnish@gmail.com>
2015-09-06 21:22:04 +0530

    makes the methods more loosely coupled and independent to facilitate easier change in the future

--- mars_rover/direction.rb
+++ mars_rover/direction.rb
@@ -5,23 +5,18 @@ class Direction
   attr_accessor :moves
 
   def initialize(path)
-    @moves = set_moves_on_successful_validation(path)
+    path = split_path_into_individual_moves(path)
+    @moves = path if movement_path_valid?(path)
   end
 
   private
 
-  def set_moves_on_successful_validation(path)
-    path = split_path_into_individual_moves(path)
-
-    unless movement_path_valid?(path)
+  def movement_path_valid?(path)
+    if path.all?{|each_move| MOVES.keys.include?(each_move)}
+      return true
+    else
       raise "Move specified as part of the path is an invalid one"
     end
-
-    path
-  end
-
-  def movement_path_valid?(path)
-    path.all?{|each_move| MOVES.keys.include?(each_move)}
   end
 
   def split_path_into_individual_moves(path)
